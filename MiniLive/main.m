bcde2800833b9677dafec7147fb43dfeb1965369
HEHE <hehe@hehedemac-mini.local>
2017-12-19 15:30:13 +0800

    Initial Commit

---
+++ MiniLive/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MiniLive
+//
+//  Created by HEHE on 2017/12/19.
+//  Copyright © 2017年 HEHE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
